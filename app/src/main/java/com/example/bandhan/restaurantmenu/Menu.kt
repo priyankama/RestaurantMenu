9418d765f16996d0c1aaea722f096515bdec40ee
priyankama <priyankamaheshwari2697@gmail.com>
2018-10-10 10:31:31 +0530

    Initial commit

---
+++ app/src/main/java/com/example/bandhan/restaurantmenu/Menu.kt
@@ -0,0 +1,7 @@
+package com.example.bandhan.restaurantmenu
+
+class Menu(val name : String, val image  : String) {
+    override fun toString(): String {
+        return name
+    }
+}
---
+++ app/src/main/java/com/example/bandhan/restaurantmenu/Services.kt
@@ -0,0 +1,4 @@
+package com.example.bandhan.restaurantmenu
+
+object Services {
+}
